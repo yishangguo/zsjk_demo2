edce008eead0fff3e186c842df92d45728791639
yishangguo <yishangguo@foxmail.com>
2018-02-25 11:09:52 +0800

    last

--- src/main/java/com/example/demo/service/TestServiceIml.java
+++ src/main/java/com/example/demo/service/TestServiceIml.java
@@ -1,14 +1,9 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 /**
  * Created by Administrator on 2018/2/21.
  */
 public class TestServiceIml implements TestService {
-
     @Override
-    public String test(String test) {
-        return "test:"+test;
-    }
+    public String test(String test) {return "test:"+test;}
 }
